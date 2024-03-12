6bfa53ed0952d3d4b36cc1d21e6b68a99b1cb92a
juansuh <juansuhr@gmail.com>
2024-03-12 13:12:10 -0400

    touchups

--- src/app.ts
+++ src/app.ts
@@ -20,21 +20,6 @@ app.get("/", (req, res) => {
   res.send("Hello Redis with Express.js and TypeScript!");
 });
 
-// Example of caching data
-app.get("/cache", async (req, res) => {
-  const cachedData = await redis.get("cachedData");
-
-  if (cachedData) {
-    // If data exists in the cache, return it
-    res.send(JSON.parse(cachedData));
-  } else {
-    // If data is not in the cache, fetch it from the source
-    const dataToCache = { message: "Data to be cached" };
-    await redis.set("cachedData", JSON.stringify(dataToCache), "EX", 3600); // Cache for 1 hour
-    res.send(dataToCache);
-  }
-});
-
 // Middleware to check if data is in the cache
 const checkCache = async (
   req: express.Request,
